ed9f6b68fcdeb64a10e8c476091e2b0c2b5ece42
jetiny <86287344@qq.com>
2016-11-18 13:32:13 +0800

    add type.js

---
+++ packages/sav-util/dist/sav-util.es.js
@@ -0,0 +1,100 @@
+/*!
+ * sav-util v0.0.1
+ * (c) 2016 jetiny 86287344@qq.com
+ * Release under the ISC License.
+ */
+function toStringType(val) {
+    return Object.prototype.toString.call(val).slice(8, -1)
+}
+
+var isArray = Array.isArray;
+
+function isBoolean(arg) {
+	return typeof arg === 'boolean'
+}
+
+function isString(arg) {
+	return typeof arg === 'string'
+}
+
+function isFunction(arg) {
+	return typeof arg === 'function'
+}
+
+function isObject(arg) {
+	return (toStringType(arg) == 'Object') && (arg !== null)
+}
+
+function isNumber(arg) {
+    return typeof arg === 'number' && !isNaN(arg)
+}
+
+function isInteger(arg) {
+    return isNumber(arg) && parseInt(arg) === arg
+}
+
+function isUndefined (arg) {
+    return arg === undefined
+}
+
+function isNull (arg) {
+    return arg === null
+}
+
+function isNan (arg) {
+    return typeof arg === 'number' && isNaN(arg)
+}
+
+function isRegExp (arg) {
+    return toStringType(arg) == 'RegExp'
+}
+
+function isDate (arg) {
+    return toStringType(arg) == 'Date'
+}
+
+function typeValue (arg) {
+    if (isNan(arg))
+        { return 'NaN' }
+    switch (arg) {
+        case undefined:
+            return 'Undefined'
+        case null:
+            return 'Null'
+        default:
+            return toStringType(arg)
+    }
+}
+
+var isInt = isInteger;
+function isUint (arg) {
+    return isInteger(arg) && arg >= 0;
+}
+
+var Test = function Test() {
+	this.test();
+};
+Test.prototype.test = function test () {
+
+};
+
+var A = (function (Test) {
+	function A () {
+		Test.apply(this, arguments);
+	}
+
+	if ( Test ) A.__proto__ = Test;
+	A.prototype = Object.create( Test && Test.prototype );
+	A.prototype.constructor = A;
+
+	A.prototype.test = function test () {
+		console.log('a');
+		// Test.prototype.test.call(this)
+	};
+
+	return A;
+}(Test));
+
+new A();
+
+export { isArray, isBoolean, isString, isFunction, isObject, isNumber, isInteger, isUndefined, isNull, isNan, isRegExp, isDate, typeValue, isInt, isUint };
---
+++ packages/sav-util/test/type.js
@@ -0,0 +1,25 @@
+import test from "ava"
+import expect from 'expect.js'
+import * as type from '../'
+
+test('type', (assert) => {
+    const items = {
+        'Object': {},
+        'Array': [],
+        'Nan': NaN,
+        'Undefined':undefined,
+        'Boolean':true,
+        'Number':1.,
+        'String':'',
+        'Function':function(){},
+        'RegExp':/text/,
+        'Date': new Date(),
+    };
+	Object.keys(items).forEach((name) =>{
+        //items
+        Object.keys(items).forEach((key) =>{
+        	let ret = type['is'+ name](items[key]);
+            expect(ret).to.be(name === key);
+        });
+    });
+});
