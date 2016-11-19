768b44e4eca06578b96581417ae8ca1fff0c1652
jetiny <86287344@qq.com>
2016-11-19 15:10:11 +0800

    test

--- packages/sav-schema/src/schema.js
+++ packages/sav-schema/src/schema.js
@@ -1,20 +1,12 @@
-/*
- * @Description     Schema第二版
- * @File 			Schema2.js
- * @Auth 			jetiny@hfjy.com
- */
+import {isString, isNumber, isObject, isArray, prop, isNull, clone } from 'sav-util'
+import * as assert from 'sav-assert'
 
-import {isString, isNumber, isObject, isArray } from '../util/type.js'
-import assert from '../assert'
-import {prop} from '../util/func.js'
-
-export const TYPE_TYPE   = 0
-export const TYPE_ENUM   = 1
-export const TYPE_STRUCT = 2
-
+const TYPE_TYPE   = 0
+const TYPE_ENUM   = 1
+const TYPE_STRUCT = 2
 const defaultTypes = []
 
-class Schema {
+export class Schema {
 	constructor() {
 		this.declare(defaultTypes)
 	}
@@ -22,6 +14,7 @@ class Schema {
 		if (isArray(opts)) {
 			return opts.map(it=>this.declare(it))
 		}
+		assert.isObject(opts)
 		let ret = createSchemaType(this, opts)
 		if (opts.name) {
 			this[opts.name] = ret
@@ -38,200 +31,234 @@ class Schema {
 	}
 }
 
-function createBasic(schema, opts) {
-	let ret = {
-		check: opts.check,
-		create: opts.create || (()=>opts.default)()
-	}
+function createType(schema, opts) {
+	const {create, check} = opts
+	assert.isFunction(create)
+	let ret = {}
+	prop(ret, 'check', check)
+	prop(ret, 'create', create || (()=>opts.default) )
 	return ret
 }
 
 function createEnum(schema, opts) {
 	const {enums, create, check} = opts
-    let values = []
-	let ret = {
-		enums,
-		values,
-        check: check || ((val) => {
-            if (values.indexOf(val) === -1) {
-                throw new Error("enum value no found:"+ JSON.stringify(val))
-            }
-        })(),
-		create: create || (()=>opts.default)()
+    let valueMaps = {}
+    let keyMaps = {}
+	let ret = {}
+	let enumList = []
+	if (isObject(enums)) {
+		for (let key in enums) {
+			let it = enums[key]
+			if (isObject(it)) {
+	    		assert.inObject(it, 'value')
+				valueMaps[keyMaps[key] = it.value] = key
+			}
+			else {
+				valueMaps[keyMaps[key] = it] = key
+			}
+		}
 	}
-    enums.forEach((it) => {
-    	values.push(ret[it.key] = it.value)
-    })
+	else if (isArray(enums)) {
+	    enums.forEach((it) => {
+	    	assert.inObject(it, 'key')
+	    	assert.inObject(it, 'value')
+			valueMaps[keyMaps[it.key] = it.value] = it.key
+	    })
+	}
+	prop(ret, 'values', valueMaps) // value=>key
+	prop(ret, 'keys', keyMaps)     // key=>value
+	prop(ret, 'check', check || ((val) => assert.inObject(valueMaps, val)))
+	prop(ret, 'create', create || (()=>opts.default))
 	return ret
 }
 
 function createSturct(schema, opts) {
-	let ret = {
+	const {props, refs, create, check} = opts
+	assert.isObject(props)
 
-	}
-	return ret
-}
-
-function createSchemaType(schema, opts) {
-	let {enums, props, refs, create, check} = opts
-	let dataType = TYPE_TYPE
-	let ret = {}
-	let fields = []
-	prop(ret, '__schema', true)
-	prop(ret, 'create', function () {
-		if (create) {
-			return create()
-		}
-		if (dataType !== TYPE_STRUCT) {
-			return opts.default
-		}
-		let ret = {};
-	    fields.forEach((it) => {
-	        ret[it.key] = it.ref.create()
-	    })
-	    return ret
-	})
-	prop(ret, 'check', function (val) {
-		if (check) {
-			return check(val)
-		}
-		if (dataType === TYPE_ENUM) {
-			if (ret.values.indexOf(val) === -1) {
-				throw new Error("enum value no found:"+ JSON.stringify(val))
-			}
-		}
-	})
-	// 引用子对象
+	let childs = {}
 	if (refs) {
 		for (let key in refs) {
 			let val = refs[key]
-			if (val.__schema) {
-				refs[key] = val
+			if (val.schema) {
+				childs[key] = val
 			}
 			else {
-				refs[key] = schema.declare(val)
+				childs[key] = schema.declare(val)
 			}
 		}
 	}
-    if (enums) { // 枚举类型
-        dataType = TYPE_ENUM
-        let values = []
-        enums.forEach((it) => {
-        	values.push(ret[it.key] = it.value)
-        })
-        prop(ret, 'enums', enums)
-        prop(ret, 'values', values)
-    } else if (props) { // 解析属性表
-        dataType = TYPE_STRUCT
-		for (let key in props) {
-			let prop = props[key]
-			const { type } = prop
-			let field = {
-				key,
+
+	let fields = []
+	for (let key in props) {
+		let field = isObject(props[key]) ? clone(props[key]) : parseProp(props[key])
+		// override [type, subType, ref, subRef, required, key]
+		const { type } = field
+	    if (type.indexOf('<') > 0) { // ["Array<User>", "Array", "User"]
+	        const mat = type.match(/^(\w+)(?:<(\w+)>)?$/);
+	        field.type    = mat[1];
+	        field.subType = mat[2];
+	        field.subRef  = childs[field.subType] || schema[field.subType]
+	    }
+	    else {
+	    	field.type = type
+	    }
+	    field.required = ('required' in field) ? field.required : !field.optional
+	    field.ref = childs[field.type] || schema[field.type]
+	    field.key = key
+		fields.push(field)
+	}
+
+	let ret = {}
+	prop(ret, 'create', create || (()=>{
+		let struct = {}
+	    fields.forEach((it) => {
+	        struct[it.key] = it.ref.create()
+	    })
+	    return struct
+	}))
+	prop(ret, 'check', check || ((value)=>{
+		try {
+		    fields.forEach((it) => {
+		        try {
+					checkStructField(ret, value, it)
+		        } catch (err) {
+		        	(err.keys || (err.keys = [])).unshift(it.key);
+            		throw err
+		        }
+		    })
+		} catch (err) {
+			if (err.keys) {
+				err.path = err.keys.join('.');
 			}
-		    if (type.indexOf('<') > 0) { // ["Array<User>", "Array", "User"]
-		        const mat = type.match(/^(\w+)(?:<(\w+)>)?$/);
-		        field.type    = mat[1];
-		        field.subType = mat[2];
-		    }
-		    else {
-		    	field.type = type
-		    }
-		    field.required = prop.required || !prop.optinal
-		    field.ref = refs[field.type] || schema[field.type]
-			fields.push(field)
+			throw err
 		}
+	}))
+	return ret
+}
+
+/**
+parseField('Number|@comment:text|@optional|len,4,10')
+=>
+{
+    "type": "Number",
+    "optional": true, 
+    "checkes": [
+        [ "len", "4", "10" ]
+    ],
+    "comment": "text"
+}
+ */
+
+const propCache = {}
+
+function parseProp(str) {
+	if (propCache[str]) {
+		return clone(propCache[str])
 	}
-	switch (dataType) {
-		case TYPE_ENUM:
-			createEnum(schema, opts)
-			break;
-		case TYPE_TYPE:
-			createBasic(schema, opts)
-			break;
+	assert.isString(str)
+    let strs = str.split('|')
+    let ret = {}
+    ret.type = strs.shift() // type first
+    ret.checkes = []
+    strs = strs.filter(function(it) {
+        if (it.length) {
+            if (it[0] == '@') {
+                let map = it.substr(1, it.length).split(':'),
+                    key = map.shift();
+                ret[lcword(key)] = map.length ? parseValue(map.shift()) : true
+                return false
+            }
+            return true
+        }
+    }).forEach(function(it) {
+        let map = it.split(':')
+        if (map.length == 1) {
+            ret.checkes.push(it.split(',')); // 或者直接使用这种模式
+            // ret.checkes.push([map.shift()])
+        } else if (map.length == 2) {
+            let key = map.shift()
+            let val = map.shift().split(',')
+            val.unshift(key)
+            ret.checkes.push(val)
+        }
+    })
+    return clone(propCache[str] = ret)
+}
+
+function lcword(s) {
+	return s.length ? s.substr(0,1).toLowerCase() + s.substr(1,s.length) : s
+}
+
+function parseValue(val) {
+    if (val == 'true')
+        return true;
+    else if (val == 'false')
+        return false;
+    let ret
+    if ((ret = parseInt(val)) == val)
+        return ret
+    else if ((ret = parseFloat(val)) == val)
+        return ret
+    return val
+}
+
+function checkStructField (struct, obj, {key, type, subType, required, nullAble, ref, checkes, subRef}) {
+	if (!required && !(key in obj)) {
+		return
 	}
+	if (nullAble && isNull(obj[key])) {
+		return
+	}
+	assert.inObject(obj, key)
+	const val = obj[key]
+	ref.check(val)
+	if (subType) {
+		assert.equal(type, 'Array') // allow Array<Struct> only
+        for (let i = 0, l = val.length; i < l; ++i) {
+            try {
+                subRef.check(val[i]);
+            } catch (err) {
+                (err.keys || (err.keys = [])).unshift(i);
+                throw err;
+            }
+        }
+	}
+}
+
+function createSchemaType(schema, opts) {
+	let dataType = TYPE_TYPE
+	let ret
+	if (opts.enums) {
+		dataType = TYPE_ENUM
+		ret = createEnum(schema, opts)
+	}
+	else if (opts.props) {
+		dataType = TYPE_STRUCT
+		ret = createSturct(schema, opts)
+	}
+	else {
+		ret = createType(schema, opts)
+	}
+	prop(ret, 'dataType', dataType)
+	prop(ret, 'schema', schema)
+	prop(ret, 'checkThen', (val) => new Promise((resolve, reject)=>{
+		try {
+			ret.check(val)
+		} catch (err) {
+			return reject(err)
+		}
+		resolve()
+	}))
 	return ret
 }
 
 Schema.register([
-	{ name: 'String',  create: String, 	check: assert.isString },
-	{ name: 'Number',  create: Number, 	check: assert.isNumber },
-	{ name: 'Array',   create: Array, 	check: assert.isArray },
-	{ name: 'Object',  create: Object, 	check: assert.isObject },
-	{ name: 'Boolean', create: Boolean, check: assert.isBoolean },
+	{ name: 'String',  	create: String, 	check: assert.isString },
+	{ name: 'Number',  	create: Number, 	check: assert.isNumber },
+	{ name: 'Array',   	create: Array, 		check: assert.isArray },
+	{ name: 'Object',  	create: Object, 	check: assert.isObject },
+	{ name: 'Boolean', 	create: Boolean, 	check: assert.isBoolean },
+	{ name: 'Int',		create: Number, 	check: assert.isInt },
+	{ name: 'Uint',		create: Number, 	check: assert.isUint },
 ])
-
-let schema = new Schema()
-
-const Sex = schema.declare({
-	default: 1,
-    enums: [
-        { key: 'male',   value: 1 },
-        { key: 'female', value: 2 }
-    ]
-})
-console.log(Sex.create(), Sex.check(2));
-
-const User = schema.declare({
-	props: {
-		name: {
-			type: 'String',
-		},
-		age: {
-			type: 'Number',
-		},
-		sex: {
-			type: 'Sex'
-		}
-	},
-	refs: {
-		Sex
-	}
-})
-console.log(User.create());
-
-const UserInfo = schema.declare({
-	props: {
-		followers: {
-			type: 'Array<User>'
-		},
-		profile: {
-			type: 'User'
-		},
-		multi: {
-			type: 'Number',
-			optional: true, // required
-			isNull: true,
-			checks: [
-				['gt', 1],
-				['lt', 10],
-			]
-		}
-	},
-	refs: {
-		User
-	}
-})
-console.log(UserInfo.create());
-
-/*
-
-{
-	Users: 'Array<User>',
-	User: {
-		name:'String',
-		age:'Number'
-	},
-}
-
-=> 
-
-{
-	name: 'Users',
-	type: 'Array',
-	subType:'User',
-}
-
-
- */
-
---
+++ packages/sav-schema/test/api.js
@@ -0,0 +1,64 @@
+import test from "ava"
+import {Schema} from '../'
+import {isFunction, isObject, isArray} from 'sav-util'
+
+const TypeProp = {
+	create (s) {
+		return 'Hello ' + s
+	},
+	default: 'Hello World',
+	check: String,
+}
+
+const SexProp = {
+	default: 1,
+    enums: { // no order
+    	male: 1,
+    	female: {
+    		value: 2,
+    		other: 'unused'
+    	},
+    }
+	// order
+	// enums: [
+	//     { key: 'male',   value: 1 },
+	//     { key: 'female', value: 2 }
+	// ]
+}
+
+const UserProp = {
+	props: {
+		name: {
+			type: 'String',
+		},
+		age: 'Number',
+		sex: 'Sex'
+	},
+	refs: {
+		Sex: SexProp
+	}
+}
+
+test("schema#api", (ava)=> {
+	ava.true(isFunction(Schema.register))
+
+	let schema = new Schema()
+	ava.true(isFunction(schema.declare))
+	// type
+	const Type = schema.declare(TypeProp)
+	ava.true(isFunction(Type.check))
+	ava.true(isFunction(Type.create))
+	ava.true(isFunction(Type.checkThen))
+	// enum
+	const Sex  = schema.declare(SexProp)
+	ava.true(isFunction(Sex.check))
+	ava.true(isFunction(Sex.create))
+	ava.true(isObject(Sex.keys))
+	ava.true(isObject(Sex.values))
+	ava.true(isFunction(Type.checkThen))
+	// struct
+	const User = schema.declare(UserProp)
+	ava.true(isFunction(User.check))
+	ava.true(isFunction(User.create))
+	ava.true(isFunction(User.checkThen))
+})
