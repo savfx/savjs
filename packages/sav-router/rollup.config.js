0f0d91f8c46c9ff1eb52020f70be1532944fa3ae
jetiny <86287344@qq.com>
2017-02-23 08:40:13 +0800

    init repo

---
+++ packages/sav-router/rollup.config.js
@@ -0,0 +1,28 @@
+import buble from 'rollup-plugin-buble'
+
+const pack = require('./package.json')
+const YEAR = new Date().getFullYear()
+
+export default {
+  entry: 'src/index.js',
+  targets: [
+    { dest: 'dist/sav-router.cjs.js', format: 'cjs' },
+    { dest: 'dist/sav-router.es.js', format: 'es' }
+  ],
+  plugins: [
+    buble()
+  ],
+  banner   () {
+    return `/*!
+ * ${pack.name} v${pack.version}
+ * (c) ${YEAR} ${pack.author.name} ${pack.author.email}
+ * Release under the ${pack.license} License.
+ */`
+  },
+  // Cleaner console
+  onwarn (msg) {
+    if (msg && msg.startsWith('Treating')) {
+      return
+    }
+  }
+}
