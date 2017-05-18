759263fd2d7d8c2055b988423ae37ee2ee43dd4b
jetiny <86287344@qq.com>
2017-05-18 22:29:50 +0800

    release

--- packages/sav-schema/rollup.config.js
+++ packages/sav-schema/rollup.config.js
@@ -1,15 +1,22 @@
-import buble from 'rollup-plugin-buble'
+import babel from 'rollup-plugin-babel'
 
 const pack = require('./package.json')
 const YEAR = new Date().getFullYear()
 
 export default {
-  entry: 'src/SchemaEnum.js',
+  entry: 'src/index.js',
   targets: [
     { dest: 'dist/sav-schema.cjs.js', format: 'cjs' }
   ],
   plugins: [
-    buble()
+    babel({
+      babelrc: false,
+      externalHelpers: false,
+      exclude: 'node_modules/**',
+      'plugins': [
+        ['transform-object-rest-spread', { 'useBuiltIns': true }]
+      ]
+    })
   ],
   banner   () {
     return `/*!
