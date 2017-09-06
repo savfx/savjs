d1a4816204ce25ba89d944580756462a8059714b
jetiny <86287344@qq.com>
2017-09-06 22:45:14 +0800

    update case

--- packages/sav-schema/test/type.js
+++ packages/sav-schema/test/type.js
@@ -58,43 +58,43 @@ test('SchemaType.export', ava => {
   expect(Age.parse).to.be.a('function')
 })
 
-// test('schema#type', ava => {
-//   const {String, Number} = schema
-//   const UserInfo = schema.declare({
-//     props: {
-//       name: String,
-//       name1: 'String',
-//       age: {
-//         type: Number
-//       },
-//       age1: 'Number',
-//       sex: 'Sex',
-//       sex1: {
-//         type: 'Sex',
-//         optional: true
-//       }
-//     },
-//     refs: {
-//       Sex: {
-//         enums: [
-//           {key: 'male', value: 1},
-//           {key: 'female', value: 2}
-//         ]
-//       }
-//     }
-//   })
-//   assert.isObject(UserInfo)
-//   assert.isNotObject(schema.Sex)
-// })
+test('schema#type', ava => {
+  const {String, Number} = schema
+  const UserInfo = schema.declare({
+    props: {
+      name: String,
+      name1: 'String',
+      age: {
+        type: Number
+      },
+      age1: 'Number',
+      sex: 'Sex',
+      sex1: {
+        type: 'Sex',
+        optional: true
+      }
+    },
+    refs: {
+      Sex: {
+        enums: [
+          {key: 'male', value: 1},
+          {key: 'female', value: 2}
+        ]
+      }
+    }
+  })
+  expect(UserInfo).to.be.a('object')
+  expect(schema.Sex).to.be.not.a('object')
+})
 
-// test('schema with raw type', ava => {
-//   const struct = schema.declare({
-//     props: {
-//       name: String,
-//       age: {
-//         type: Number
-//       }
-//     }
-//   })
-//   assert.isObject(struct)
-// })
+test('schema with raw type', ava => {
+  const struct = schema.declare({
+    props: {
+      name: String,
+      age: {
+        type: Number
+      }
+    }
+  })
+  expect(struct).to.be.a('object')
+})
